a7177d78479b4aa230dadd5b496283187eaea37d
Adem ÖZER <58594799+160201024@users.noreply.github.com>
2021-06-28 16:07:12 +0300

    refactoring

--- src/app/crypto.ts
+++ src/app/crypto.ts
@@ -1,32 +1,32 @@
 import {Buffer} from 'buffer';
-import * as CryptoJS from 'crypto-js';
+import {enc,AES, mode, pad} from 'crypto-js';
 
 export function decrypt(encryptedStr: string, key: string): string {
   const IV = new Buffer([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const encryptKey = CryptoJS.enc.Utf8.parse(key);
-  const bytes = CryptoJS.AES.decrypt(
+  const encryptKey = enc.Utf8.parse(key);
+  const bytes = AES.decrypt(
     encryptedStr.toString(),
     encryptKey,
     {
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-      iv: CryptoJS.enc.Utf8.parse(IV.toString()),
+      mode: mode.CBC,
+      padding: pad.Pkcs7,
+      iv: enc.Utf8.parse(IV.toString()),
     },
   );
 
-  return CryptoJS.enc.Utf8.stringify(bytes);
+  return enc.Utf8.stringify(bytes);
 }
 
 export function encrypt(val: string, key: string): string {
   const IV = new Buffer([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const encryptKey = CryptoJS.enc.Utf8.parse(key);
-  const encryptedStr = CryptoJS.AES.encrypt(
+  const encryptKey = enc.Utf8.parse(key);
+  const encryptedStr = AES.encrypt(
     val,
     encryptKey,
     {
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
-      iv: CryptoJS.enc.Utf8.parse(IV.toString()),
+      mode: mode.CBC,
+      padding: pad.Pkcs7,
+      iv: enc.Utf8.parse(IV.toString()),
     },
   );
   return encryptedStr.toString();
